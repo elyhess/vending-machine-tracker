dbe31c811bc70fe272a34c55121759ac7e73979a
Ely Hess <Ely.hess@me.com>
2020-12-31 15:23:06 -0700

    Add average_price test

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -39,7 +39,24 @@ RSpec.describe "When a user visits a vending machine show page", type: :feature 
       expect(all('.snack-name')[0].text).to eq(snack_3.name)
       expect(all('.snack-price')[0].text).to eq(snack_3.price.to_s)
     end
+  end
 
+  it 'it shows the average price of all snacks for that machine' do
+    sam = Owner.create(name: "Sam's Snacks")
+    machine_1 = sam.machines.create(location: "Don's Mixed Drinks")
+    machine_2 = sam.machines.create(location: "Turing Basement")
+
+    snack_1 = Snack.create(name: "Cheezit-Grooves", price: 350)
+    snack_2 = Snack.create(name: "Cheezit-razorblades", price: 300)
+    snack_3 = Snack.create(name: "Cheezit-Smooths", price: 400)
+
+    SnackMachine.create(snack: snack_1, machine: machine_1)
+    SnackMachine.create(snack: snack_2, machine: machine_1)
+    SnackMachine.create(snack: snack_3, machine: machine_1)
+
+    visit machine_path(machine_1)
+
+    expect(page).to have_content("Average snack price: #{Snack.average_price}")
   end
 
 
