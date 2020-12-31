2d11c14443b8bf02d295e5566dc960af224bc915
Ely Hess <Ely.hess@me.com>
2020-12-31 15:20:42 -0700

    Add average_price spec

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Snack, type: :model do
 
   describe 'instance methods' do
   end
+
+  describe "class methods" do
+    it '::average_price' do
+      sam = Owner.create(name: "Sam's Snacks")
+      machine_1 = sam.machines.create(location: "Don's Mixed Drinks")
+  
+      snack_1 = Snack.create(name: "Cheezit-Grooves", price: 350)
+      snack_2 = Snack.create(name: "Cheezit-razorblades", price: 300)
+      snack_3 = Snack.create(name: "Cheezit-Smooths", price: 400)
+  
+      SnackMachine.create(snack: snack_1, machine: machine_1)
+      SnackMachine.create(snack: snack_2, machine: machine_1)
+      SnackMachine.create(snack: snack_3, machine: machine_1)
+      
+      expect(Snack.average_price).to eq(350)
+    end
+  end 
 end
